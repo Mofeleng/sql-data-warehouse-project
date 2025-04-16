a26ef3737c07256c5a8ec4cea1f4a59a506e67e7
Mofeleng <145613816+Mofeleng@users.noreply.github.com>
2025-04-16 13:40:44 +0200

    Update load_crm_prd_info_clean.sql

--- scripts/silver/load_crm_prd_info_clean.sql
+++ scripts/silver/load_crm_prd_info_clean.sql
@@ -30,8 +30,8 @@ INSERT INTO silver.crm_prd_info (
 )
 SELECT 
 	prd_id,
-	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
 	SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key, -- used in sales_details
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
 	prd_nm,
 	ISNULL(prd_cost, 0) AS prd_cost,
 	CASE UPPER(TRIM(prd_line))
@@ -44,5 +44,3 @@ SELECT
 	CAST (prd_start_dt AS DATE) AS prd_start_dt,
 	CAST (LEAD (prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) AS prd_end_dt
 FROM bronze.crm_prd_info
-
-SELECT * FROM silver.crm_prd_info
